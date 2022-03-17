60f7aa811f37a125043179501fa98acbf1030a14
Tigran Drmeyan <drmeyant@gmail.com>
2022-03-17 20:17:30 +0400

    Initial commit

---
+++ src/main/java/com/example/tmpelectronics/TmpElectronicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmpelectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpElectronicsApplication.class, args);
+	}
+
+}
