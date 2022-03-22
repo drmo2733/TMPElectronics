8530a32b7028f1935e97a2df49c5ac05298f8483
Tigran Drmeyan <drmeyant@gmail.com>
2022-03-22 21:01:10 +0400

    add security partly

---
+++ src/main/java/com/example/tmpelectronics/config/SpringSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.tmpelectronics.config;
+
+import com.example.tmpelectronics.security.UserDetailsImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsImpl userDetails;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetails)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
