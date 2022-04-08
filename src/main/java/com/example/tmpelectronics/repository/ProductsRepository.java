0dad56b5a80be11784f25f12a4771a0d22b481ba
Tigran Drmeyan <drmeyant@gmail.com>
2022-04-08 22:12:12 +0400

    html changes + add photos

--- src/main/java/com/example/tmpelectronics/repository/ProductsRepository.java
+++ src/main/java/com/example/tmpelectronics/repository/ProductsRepository.java
@@ -1,10 +1,7 @@
 package com.example.tmpelectronics.repository;
 
 import com.example.tmpelectronics.entity.Products;
-import com.example.tmpelectronics.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ProductsRepository extends JpaRepository<Products, Integer> {
 
